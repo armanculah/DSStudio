ba088210245606810afe46df5a7cebb19df7b006
Arman Ćulah <arman.culah@stu.ibu.edu.ba>
2025-11-12 13:49:56 +0100

    Frontend: Vite and Tailwind first setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./views/**/*.html", "./src/**/*.{js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
