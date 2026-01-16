24993c34c79130b6468cfc7daedeb5c3ddf77bf7
Arman Ćulah <arman.culah@stu.ibu.edu.ba>
2026-01-16 13:23:29 +0100

    Host: Frotnend routing 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   server: {
     proxy: {
       "/api": "http://localhost:8000",
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        "views/login": resolve(__dirname, "views/login.html"),
+        "views/signup": resolve(__dirname, "views/signup.html"),
+        "views/playground": resolve(__dirname, "views/playground.html"),
+        "views/profile": resolve(__dirname, "views/profile.html"),
+      },
     },
   },
 });
